b8073db10882ba797b0f6574c61c8ecb7d62c824
Donald <donfrozex@gmail.com>
2025-01-17 15:56:21 +0100

    add logo. move styles to a separate file.

--- matrixProject/settings.py
+++ matrixProject/settings.py
@@ -107,7 +107,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
