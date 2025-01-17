cba025fbcb7345ef54d73eb9a2179c5dbe0f74de
Dimitriovski Donald <donfrozex@gmail.com>
2025-01-17 23:31:16 +0100

    remove dummy sentry error view

--- matrixProject/urls.py
+++ matrixProject/urls.py
@@ -4,12 +4,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
 urlpatterns = [
     path('', include('matrix.urls' )),
     path('admin/', admin.site.urls),
-    path('sentry-debug/', trigger_error),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
