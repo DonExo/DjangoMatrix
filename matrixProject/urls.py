8bfbe7266a073f81d5f642dc2f325e9efb6f3efa
Donald <donfrozex@gmail.com>
2025-01-28 12:31:22 +0100

    reorganize templates. added about.html

--- matrixProject/urls.py
+++ matrixProject/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from matrixProject.settings import ADMIN_URL_PATH
 
 urlpatterns = [
     path('', include('matrix.urls')),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path(ADMIN_URL_PATH, admin.site.urls),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- static/js/matrix.js
+++ static/js/matrix.js
@@ -1,13 +1,19 @@
-    document.addEventListener('DOMContentLoaded', function () {
-        // Initialize Bootstrap Tooltips
-        const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-        tooltipTriggerList.map(function (tooltipTriggerEl) {
-            return new bootstrap.Tooltip(tooltipTriggerEl);
-        });
-        // Initialize Bootstrap Toast messages
-        var toastElements = document.querySelectorAll('.toast');
-        toastElements.forEach(function (toastElement) {
-            var toast = new bootstrap.Toast(toastElement);
-            toast.show();
-        });
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Initialize Bootstrap Tooltips
+    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
     });
+
+    // Initialize Bootstrap Toast messages
+    var toastElements = document.querySelectorAll('.toast');
+    toastElements.forEach(function (toastElement) {
+        var toast = new bootstrap.Toast(toastElement);
+        toast.show();
+    });
+
+
+});
+
+
